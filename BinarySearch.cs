2bf087a8f1cd1f5c7ffe72c1c65ec41316162ec5
ramyashrict <ramyarr1415@gmail.com>
2023-04-04 19:58:27 +0530

    added data strucher programs

---
+++ BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucher
+{
+    class BinarySearch
+    {
+        static void Main()
+        {
+            var data = new List<string>() { "banana", "peach", "apple" };
+            data.Sort();
+            Console.WriteLine(string.Join(",", data));
+            int i = data.BinarySearch("peach");
+            Console.WriteLine(i);
+
+            i = data.BinarySearch("banana");
+            Console.WriteLine(i);
+
+            i = data.BinarySearch("apple");
+            Console.WriteLine(i);
+        }
+    }
+}
